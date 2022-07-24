a7ba937ce836a1d4041bdd58488400797b974257
suk c4 <suk c4@ADMINRG-TN51OJ3>
2022-07-25 02:02:12 +0530

    commit1

---
+++ meth/src/meth/m1.java
@@ -0,0 +1,20 @@
+package meth;
+
+public class m1 {
+	int a,b;
+	static int sum;
+	m1(int a, int b){
+	
+	sum=a+b;
+	}
+public static void main(String[] args) {
+	m1 ob=new m1(10, 20);
+	ob(10,20);
+	System.out.println(sum);
+}
+private static void ob(int i, int j) {
+	// TODO Auto-generated method stub
+	
+}
+
+}
